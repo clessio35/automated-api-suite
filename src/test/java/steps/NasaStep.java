54a2cff4c41658b35625fef1b2a3c05aae6e5a42
Clessio <clessio.silva1989@gmail.com>
2025-08-11 18:56:57 -0300

    nasa steps

---
+++ src/test/java/steps/NasaStep.java
@@ -0,0 +1,44 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NasaStep {
+	
+	@When("realizo uma request GET para {string} com uma chave de API valida")
+	public void realizo_uma_request_get_para_com_uma_chave_de_api_valida(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido que a resposta contém os dados da imagem do dia")
+	public void eu_valido_que_a_resposta_contém_os_dados_da_imagem_do_dia() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("realizo uma request GET para {string} com o parametro {string} igual a {string}")
+	public void realizo_uma_request_get_para_com_o_parametro_igual_a(String string, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido que a resposta contém as fotos tiradas nessa data")
+	public void eu_valido_que_a_resposta_contém_as_fotos_tiradas_nessa_data() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("realizo uma request GET para {string} com a data inicial {string} e final {string}")
+	public void realizo_uma_request_get_para_com_a_data_inicial_e_final(String string, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido que a resposta contém dados dos objetos próximos à Terra nesse período")
+	public void eu_valido_que_a_resposta_contém_dados_dos_objetos_próximos_à_terra_nesse_período() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
